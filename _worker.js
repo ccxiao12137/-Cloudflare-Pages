b6b5552df3ad99a3647267e438b711af60cb4e23
ccxiao12137 <147781292+ccxiao12137@users.noreply.github.com>
2023-12-28 09:42:43 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'vf.ccpupu.cf'
+      url.hostname = 'vf.ccxiao12137.workers.dev'
 //       url.hostname = 'youtube.com'
       let new_request = new Request(url, request);
       return fetch(new_request);
